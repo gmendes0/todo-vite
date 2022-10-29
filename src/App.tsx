31c2df340ebd75f8b4527984d87dc7ac8cc7b7a2
gxbriel9g <gmendes230@gmail.com>
2022-10-29 18:28:04 -0300

    feat: create/delete/check tasks

--- src/App.tsx
+++ src/App.tsx
@@ -3,58 +3,112 @@ import styles from "./App.module.scss";
 import { Logo } from "./components/Logo";
 import { ListHeader } from "./components/ListHeader";
 import { Task } from "./components/Task";
+import { ChangeEvent, FormEvent, InvalidEvent, useMemo, useState } from "react";
+import { EmptyList } from "./components/EmptyList";
+import { v4 as uuidv4 } from "uuid";
+
+interface Task {
+  id: string;
+  description: string;
+  isCompleted: boolean;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: "1", description: "Fazer compras", isCompleted: false },
+    {
+      id: "2",
+      description: "Finalizar o módulo 1 de ReactJS ",
+      isCompleted: true,
+    },
+  ]);
+
+  const [newTask, setNewTask] = useState<string>("");
+
+  const completedTasks = useMemo(
+    () => tasks.reduce((acc, task) => acc + (task.isCompleted ? 1 : 0), 0),
+    [tasks]
+  );
+
+  const totalTasksCount = tasks.length;
+
+  function handleNewTaskInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+
+    event.target.setCustomValidity("");
+  }
+
+  function handleCreateNewTask(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    setTasks((state) => [
+      ...state,
+      { id: uuidv4(), description: newTask, isCompleted: false },
+    ]);
+
+    setNewTask("");
+  }
+
+  function handleInvalidNewTaskInput(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity("Por favor, digite algo");
+  }
+
+  // callback
+  function handleToggleTaskIsCompleted(id: string) {
+    setTasks((state) =>
+      state.map((task) =>
+        task.id === id
+          ? { ...task, isCompleted: !task.isCompleted }
+          : { ...task }
+      )
+    );
+  }
+
+  // callback
+  function handleDeleteTask(id: string) {
+    console.log(tasks.filter((task) => task.id !== id));
+
+    setTasks((state) => state.filter((task) => task.id !== id));
+  }
+
   return (
     <div>
       <Logo />
 
       <main className={styles.container}>
-        <form className={styles.form}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
+        <form className={styles.form} onSubmit={handleCreateNewTask}>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            onChange={handleNewTaskInputChange}
+            value={newTask}
+            required
+            onInvalid={handleInvalidNewTaskInput}
+          />
           <button>
             Criar <PlusCircle weight="bold" />
           </button>
         </form>
 
-        <ListHeader totalTasksCount={15} completedTasksCount={7} />
+        <ListHeader
+          totalTasksCount={totalTasksCount}
+          completedTasksCount={completedTasks}
+        />
 
-        {/* <EmptyList /> */}
-
-        <div>
-          <Task
-            id="1"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          />
-          <Task
-            id="2"
-            description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta exercitationem officia placeat dolorem excepturi sunt atque voluptate porro. Esse repellendus, non quasi quibusdam magnam quam fuga ipsam temporibus sint deserunt?"
-          />
-          <Task
-            id="2"
-            description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta exercitationem officia placeat dolorem excepturi sunt atque voluptate porro. Esse repellendus, non quasi quibusdam magnam quam fuga ipsam temporibus sint deserunt?"
-          />
-          <Task
-            id="1"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          />
-          <Task
-            id="2"
-            description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta exercitationem officia placeat dolorem excepturi sunt atque voluptate porro. Esse repellendus, non quasi quibusdam magnam quam fuga ipsam temporibus sint deserunt?"
-          />
-          <Task
-            id="2"
-            description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta exercitationem officia placeat dolorem excepturi sunt atque voluptate porro. Esse repellendus, non quasi quibusdam magnam quam fuga ipsam temporibus sint deserunt?"
-          />
-          <Task
-            id="1"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          />
-          <Task
-            id="2"
-            description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta exercitationem officia placeat dolorem excepturi sunt atque voluptate porro. Esse repellendus, non quasi quibusdam magnam quam fuga ipsam temporibus sint deserunt?"
-          />
-        </div>
+        {totalTasksCount === 0 ? (
+          <EmptyList />
+        ) : (
+          <div>
+            {tasks.map((task) => (
+              <Task
+                key={task.id}
+                task={task}
+                onToggleIsCompleted={handleToggleTaskIsCompleted}
+                onDeleteTask={handleDeleteTask}
+              />
+            ))}
+          </div>
+        )}
       </main>
     </div>
   );
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,20 +1,40 @@
 import { Trash } from "phosphor-react";
+import { ChangeEvent } from "react";
 import styles from "./Task.module.scss";
 
-interface TaskProps {
-  description: string;
+interface Task {
   id: string;
+  description: string;
+  isCompleted: boolean;
+}
+interface TaskProps {
+  task: Task;
+  onToggleIsCompleted: (id: string) => void;
+  onDeleteTask: (id: string) => void;
 }
 
-export function Task({ id, description }: TaskProps) {
+export function Task({ task, onToggleIsCompleted, onDeleteTask }: TaskProps) {
+  function handleToggleIsCompleted(id: string) {
+    onToggleIsCompleted(id);
+  }
+
+  function handleDeleteTask(id: string) {
+    onDeleteTask(id);
+  }
+
   return (
     <div className={styles.task}>
       <div className={styles.checkboxControl}>
-        <input id={`checkbox-${id}`} type="checkbox" />
-        <label htmlFor={`checkbox-${id}`} />
+        <input
+          id={`checkbox-${task.id}`}
+          type="checkbox"
+          checked={task.isCompleted}
+          onChange={() => handleToggleIsCompleted(task.id)}
+        />
+        <label htmlFor={`checkbox-${task.id}`} />
       </div>
-      <p>{description}</p>
-      <button>
+      <p>{task.description}</p>
+      <button onClick={() => handleDeleteTask(task.id)}>
         <Trash size={14} weight="bold" />
       </button>
     </div>
